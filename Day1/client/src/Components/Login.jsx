432383fea645113b254752545f0977e8c67672c2
sandeepmore-code <152766192+sandeepmore-code@users.noreply.github.com>
2024-05-14 14:40:09 +0530

    updates

--- Day1/client/src/Components/Login.jsx
+++ Day1/client/src/Components/Login.jsx
@@ -1,30 +1,98 @@
-import { useState } from "react";
+// import { useState } from "react";
+// import toast from "react-hot-toast";
+// import {  useNavigate } from "react-router-dom";
 
-function Login (){
-  const [user,setUserData]=useState({email :"",password :""});
-  console.log(user,"user");
-  function handleChange(event){
-    setUserData({[event.target.name]: event.target.value})
-  }
+// function Login (){
+//   const router = useNavigate();
+//   const [user,setUserData]= useState({email :"",password :""});
+//   console.log(user,"userData");
+//   function handleChange(event){
+//     setUserData({,[event.target.name]:event.target.value})
+//   }
 
-  // function handlesubmit(event){
-  //   event.preventdefault();
-      
-  // }
-
-return(
+//   async function handlesubmit(event){
+//     event.preventDefault();
+//     if(user.email && user.password){
+//       try{
+//         const response = {data:{success : true , message : "Login Successful"}}
+//         if(response.data.success){
+//           setUserData({email : "",password : ""})
+//           toast.success(response.data.message)
+//           router('/')
+//         }
+//       }catch(error){
+//         toast.error(error.response.data.message)  
+//       }
+//     }else{
+//       alert("all fields are required")
+//     }
+//   }
+// return(
 
   
-  <div>
-    <h1><b><u>Login</u></b></h1>
-    <form onChange={handleChange}>
-      <label>Email</label><br/>
-      <input type="email" name="email" required/><br/>
-      <label>Password</label><br/>
-      <input type="password" name="password" required/><br/><br/>
-      <input type="submit" value="Login"/>
-    </form>
-  </div>
-)
+//   <div>
+//     <h1><b><u>Login</u></b></h1>
+//     <form onChange={handlesubmit}>
+//       <label>Email</label><br/>
+//       <input type="email" name="email" onChange={handleChange} required/><br/>
+//       <label>Password</label><br/>
+//       <input type="password" name="password" onChange={handleChange} required/><br/><br/>
+//       <input type="submit" value="Login"/>
+//     </form>
+//   </div>
+// )
+// }
+// export default Login;
+
+
+
+
+import { useState } from "react";
+import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
+
+function Login() {
+  const router = useNavigate();
+  const [userData, setUserData] = useState({ email: "", password: "" });
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setUserData((prevUserData) => ({
+      ...prevUserData,
+      [name]: value,
+    }));
+  }
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    if (userData.email && userData.password) {
+      try {
+        const response = { data: { success: true, message: "Login Successful" } };
+        if (response.data.success) {
+          setUserData({ email: "", password: "" });
+          toast.success(response.data.message);
+          router('/');
+        }
+      } catch (error) {
+        toast.error(error.response.data.message);
+      }
+    } else {
+      alert("All fields are required");
+    }
+  }
+
+  return (
+    <div>
+      <h1><b><u>Login</u></b></h1>
+      <form onSubmit={handleSubmit}>
+        <label>Email</label><br/>
+        <input type="email" name="email" value={userData.email} onChange={handleChange} required/><br/>
+        <label>Password</label><br/>
+        <input type="password" name="password" value={userData.password} onChange={handleChange} required/><br/><br/>
+        <input type="submit" value="Login"/>
+      </form>
+    </div>
+  );
 }
+
 export default Login;
