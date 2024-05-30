b748527defe211b0989a069b178498508b449926
sandeepmore-code <152766192+sandeepmore-code@users.noreply.github.com>
2024-05-31 03:52:28 +0530

    updates

--- Day1/client/src/Components/09-05/AddProduct.jsx
+++ Day1/client/src/Components/09-05/AddProduct.jsx
@@ -1,37 +1,53 @@
 import React, { useContext, useState } from "react";
 import { AuthContext } from "../10-03/context/AuthContext";
 import AuthDirection from "../Redirections/AuthDirects";
+import axios from "axios";
+import toast from "react-hot-toast";
 
 const AddProduct=()=>{
   const [productData, setProductData] = useState({name : "",category:"",price :"",quantity: "",tags: ""})
+  console.log(productData,"productData")
   const handleChange=(event)=>{
-setProductData({[event.target.name] : event})
+setProductData({...productData,[event.target.name] : event.target.value})
   }
   const {state} = useContext(AuthContext); 
   console.log(state,"state");
+  const handleSubmit = async(event)=>{
+    try{
+      event.preventDefault();
+    const response = await axios.post('http://localhost:3001/add-product', {productData, userid : "6658591fbe31cba681ef9413"});
+    if(response.data.success){
+      toast.success(response.data.message)
+    }
+    }catch(error){
+      toast.error(error.response.data.error)
+    }
+  }
+  
   return(
     <div>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>Product Name</label>
         <br/>
-        <input name="name"/>
+        <input required name="name" onChange={handleChange}/>
+        <br/>
         <label>Product category</label>
         <br/>
-        <input name="category"/>
-        <label>Product Name</label>
+        <input required name="category" onChange={handleChange}/>
         <br/>
-        <input name="price"/>
         <label>Product Price</label>
         <br/>
-        <input name="quantity"/>
-        <label>Product quantity</label>
+        <input required name="price" onChange={handleChange}/>
         <br/>
-        <input name="quantity"/>
+        <label>Product Quantity</label>
+        <br/>
+        <input required name="quantity" onChange={handleChange}/>
+        <br/>
         <label>Product Tags</label>
         <br/>
-        <input name="tags"/>
+        <input required name="tags"onChange={handleChange}/>
         <br/>
-        <input type="submit"/>
+        <input required  type="submit"/>
       </form>
     </div>
   )
