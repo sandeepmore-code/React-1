fac3cb2d8ebbed579d2fc68661bc63e8f6ab4860
sandeepmore-code <152766192+sandeepmore-code@users.noreply.github.com>
2024-06-05 02:13:21 +0530

    updates

--- Day1/client/src/AxiosConfig/index.js
+++ Day1/client/src/AxiosConfig/index.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const api = axios.create({
-  baseURL: "http://localhost:3001" 
+  baseURL: "https://backendnode-learning.vercel.app/" 
 //   process.env.ENV === "DEVLOPMENT"? "http://localhost:3001":
 // "https://awdiz-6.com"
 ,  withCredentials: true, 
