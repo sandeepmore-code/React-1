f8084b2a199c5abc80ce7e7839d6c90fffee6ed7
sandeepmore-code <152766192+sandeepmore-code@users.noreply.github.com>
2024-06-06 14:44:23 +0530

    updates

--- Day1/client/src/Components/10-03/context/AuthContext.js
+++ Day1/client/src/Components/10-03/context/AuthContext.js
@@ -1,55 +1,108 @@
-import axios from "axios";
+// import axios from "axios";
+// import { createContext, useEffect, useReducer } from "react";
+// import api from "../../../AxiosConfig";
+// // import { json } from "react-router-dom";
+
+
+
+// export const AuthContext = createContext();
+
+// function Reducer (state, action){
+//   switch(action.type){
+//     case "LOGIN":
+//       return {...state,user : action.payload}
+//     case "LOGOUT": 
+//       return {...state,user : null}
+      
+//       default : 
+//        return state;
+//   }
+// }
+
+// const Initialstate = {user : null}
+
+// const AuthContextComponent=({children})=>{
+//   const [state ,dispatch] = useReducer(Reducer,Initialstate)
+
+//   function LOGIN (data){
+//     dispatch({type : "LOGIN" , payload : data})
+//   }
+//   function LOGOUT (){
+//     dispatch({type : "LOGOUT"})
+//   }
+//  async function getUserData(){
+//   try{
+//     const response = await api.get("/api/v1/user/validate-token")
+//     // const response = { data: { success: true, userData:{name : 'awdiz', email : " awdiz@gmail.com"} } };
+//     if(response.data.success){
+//       LOGIN(response.data.user)
+//     }
+//   }catch(error){
+//     console.log(error)
+//   }
+//  }
+//   useEffect(()=>{
+//     // const token = JSON.parse(localStorage.getItem("token"))
+//     getUserData()
+//   },[])
+//   return(
+//     <AuthContext.Provider value={{state,LOGIN,LOGOUT}}>
+//       {children}
+//     </AuthContext.Provider>
+//   )
+// }
+
+// export default AuthContextComponent;
+
 import { createContext, useEffect, useReducer } from "react";
 import api from "../../../AxiosConfig";
-// import { json } from "react-router-dom";
-
-
 
 export const AuthContext = createContext();
 
-function Reducer (state, action){
-  switch(action.type){
+function reducer(state, action) {
+  switch (action.type) {
     case "LOGIN":
-      return {...state,user : action.payload}
-    case "LOGOUT": 
-      return {...state,user : null}
-      
-      default : 
-       return state;
+      return { ...state, user: action.payload };
+    case "LOGOUT":
+      return { ...state, user: null };
+    default:
+      return state;
   }
 }
 
-const Initialstate = {user : null}
+const initialState = { user: null };
 
-const AuthContextComponent=({children})=>{
-  const [state ,dispatch] = useReducer(Reducer,Initialstate)
+const AuthContextComponent = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
 
-  function LOGIN (data){
-    dispatch({type : "LOGIN" , payload : data})
-  }
-  function LOGOUT (){
-    dispatch({type : "LOGOUT"})
-  }
- async function getUserData(){
-  try{
-    const response = await api.get("/api/v1/user/validate-token")
-    // const response = { data: { success: true, userData:{name : 'awdiz', email : " awdiz@gmail.com"} } };
-    if(response.data.success){
-      LOGIN(response.data.user)
+  const login = (data) => {
+    dispatch({ type: "LOGIN", payload: data });
+  };
+
+  const logout = () => {
+    dispatch({ type: "LOGOUT" });
+  };
+
+  const getUserData = async () => {
+    try {
+      const response = await api.get("/api/v1/user/validate-token");
+      if (response.data.success) {
+        login(response.data.user);
+      }
+    } catch (error) {
+      console.error("Error fetching user data:", error);
     }
-  }catch(error){
-    console.log(error)
-  }
- }
-  useEffect(()=>{
-    // const token = JSON.parse(localStorage.getItem("token"))
-    getUserData()
-  },[])
-  return(
-    <AuthContext.Provider value={{state,LOGIN,LOGOUT}}>
+  };
+
+  useEffect(() => {
+    getUserData();
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ state, login, logout }}>
       {children}
     </AuthContext.Provider>
-  )
-}
+  );
+};
 
 export default AuthContextComponent;
