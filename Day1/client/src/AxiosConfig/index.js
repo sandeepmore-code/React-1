6014745b4f7efcb625471fc2dc6e9fed2f6c5128
sandeepmore-code <152766192+sandeepmore-code@users.noreply.github.com>
2024-06-10 02:22:40 +0530

    updates

--- Day1/client/src/AxiosConfig/index.js
+++ Day1/client/src/AxiosConfig/index.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const api = axios.create({
-  baseURL: "https://backend-node-o46f.onrender.com" 
+  baseURL: "https://backend-node-f8ey.onrender.com" 
 //   process.env.ENV === "DEVLOPMENT"? "http://localhost:3001":
 // "https://awdiz-6.com"
 ,  withCredentials: true, 
