141292fe57c800229639444c458fca748652cae5
sandeepmore-code <152766192+sandeepmore-code@users.noreply.github.com>
2024-06-09 15:59:34 +0530

    update

--- Day1/client/src/AxiosConfig/index.js
+++ Day1/client/src/AxiosConfig/index.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const api = axios.create({
-  baseURL: "" 
+  baseURL: "https://backend-node-4bzk.onrender.com" 
 //   process.env.ENV === "DEVLOPMENT"? "http://localhost:3001":
 // "https://awdiz-6.com"
 ,  withCredentials: true, 
